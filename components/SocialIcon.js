106aeb0fa7970bee3b16877ef0e204042435be24
priyam125 <sinha.priyam002@gmail.com>
2024-08-13 13:12:37 +0530

    fix

--- components/SocialIcon.js
+++ components/SocialIcon.js
@@ -11,27 +11,27 @@ import { motion } from "framer-motion";
 
 function SocialIcons() {
   const socialLinks = [
-    { name: "Github", icon: <FiGithub />, link: "https://www.github.com" },
-    {
-      name: "Youtube",
-      icon: <FiYoutube />,
-      link: "https://www.youtube.com/KishanSheth21/",
-    },
+    { name: "Github", icon: <FiGithub />, link: "https://github.com/priyam125" },
+    // {
+    //   name: "Youtube",
+    //   icon: <FiYoutube />,
+    //   link: "https://www.youtube.com/KishanSheth21/",
+    // },
     {
       name: "LinkedIn",
       icon: <FiLinkedin />,
-      link: "https://www.linkedin.com/in/koolkishan/",
+      link: "https://www.linkedin.com/in/priyam125/",
     },
-    {
-      name: "Instagram",
-      icon: <FiInstagram />,
-      link: "https://instagram.com/koolkishansheth",
-    },
-    {
-      name: "Twitter",
-      icon: <FiTwitter />,
-      link: "https://twitter.com/koolkishansheth",
-    },
+    // {
+    //   name: "Instagram",
+    //   icon: <FiInstagram />,
+    //   link: "https://instagram.com/koolkishansheth",
+    // },
+    // {
+    //   name: "Twitter",
+    //   icon: <FiTwitter />,
+    //   link: "https://twitter.com/koolkishansheth",
+    // },
   ];
   return (
     <div className="social-icons">
